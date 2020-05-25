c33d0a27b45ce6f424dcf40a0fffea630492780a
unknown <rjoealexander@gmail.com>
2020-05-26 00:41:32 +0530

    rjoealex commit 1

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Hero() {
+  return (
+  
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <img src="library_cover.jpeg" alt="AK Library slider" />
+        </div>
+
+      
+
+
+      </div>
+   
+</section>
+  )
+}
