1f20c4b74d0c0a4b1a27f55b66dd675136edc952
unknown <rjoealexander@gmail.com>
2020-05-27 00:45:28 +0530

    rjoealex home page commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,53 +3,53 @@ import React from 'react';
 export default function Hero() {
   return (
   
-    <section id="home" class="slider" data-stellar-background-ratio="0.5">
-    <div class="container">
-         <div class="row">
-
-                   <div class="owl-carousel owl-theme">
-                        <div class="item item-first" >
+     <section id="home" class="slider" data-stellar-background-ratio="0.5">
+     <div class="container">
+          <div class="row">
+ 
+                    <div class="owl-carousel owl-theme">
+                         <div class="item item-first" >
+                              
                              
-                            
+                              
+                              <div class="caption">
+                                   <div class="col-md-offset-1 col-md-10">
+                                        <h3>Let's make your life happier</h3>
+                                        <h1>Healthy Living</h1>
+                                        <a href="#team" class="section-btn btn btn-default smoothScroll">Meet Our Doctors</a>
+                                   </div>
+                              </div>
+                         
+                         </div>
+ 
+                         <div class="item item-second" >
+                              
                              
-                             <div class="caption">
-                                  <div class="col-md-offset-1 col-md-10">
-                                       <h3>Let's make your life happier</h3>
-                                       <h1>Healthy Living</h1>
-                                       <a href="#team" class="section-btn btn btn-default smoothScroll">Meet Our Doctors</a>
-                                  </div>
-                             </div>
-                        
-                        </div>
-
-                        <div class="item item-second" >
-                             
-                            
-                             
-                             <div class="caption">
-                                  <div class="col-md-offset-1 col-md-10">
-                                       <h3>Let's make your life happier</h3>
-                                       <h1>Healthy Living</h1>
-                                       <a href="#team" class="section-btn btn btn-default smoothScroll">Meet Our Doctors</a>
-                                  </div>
-                             </div>
-                        
-                        </div>
-
-
-
-                      
-
-
-
-
-
-
-
-                   </div>
-
-         </div>
-    </div>
-</section>
+                              
+                              <div class="caption">
+                                   <div class="col-md-offset-1 col-md-10">
+                                        <h3>Let's make your life happier</h3>
+                                        <h1>Healthy Living</h1>
+                                        <a href="#team" class="section-btn btn btn-default smoothScroll">Meet Our Doctors</a>
+                                   </div>
+                              </div>
+                         
+                         </div>
+ 
+ 
+ 
+                       
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+                    </div>
+ 
+          </div>
+     </div>
+ </section>
   )
 }
