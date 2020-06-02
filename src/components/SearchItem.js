10422c22ec6a6dad66169b7d62433a5ff03ca85f
rjoealexsj <rjoealexander@gmail.com>
2020-06-02 11:43:57 +0530

    Search corrected data display

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Bookquery from './Bookquery';
+
 
 
 export default class SearchItem extends Component {
   render() {
     return (
-      
+      <div>
+
+      </div>
     )
   }
 }
