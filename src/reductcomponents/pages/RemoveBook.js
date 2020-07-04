d2f936a0e67c1dbfe70603a746ab3043e76d818f
rjoealexsj <rjoealexander@gmail.com>
2020-07-04 10:49:02 +0530

    book issue front design

---
+++ src/reductcomponents/pages/RemoveBook.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Bookquery from '../../components/Bookquery';
+
+//import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {Table} from 'react-bootstrap';
+
+
+import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
+    Form,
+    FormFeedback,
+    FormGroup,
+    FormText,
+    Input,
+    Label,
+    Row,
+  } from 'reactstrap';
+  const userquery='';
+
+  
+const config = require('../../config.json');
+export default class RemoveBook extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
