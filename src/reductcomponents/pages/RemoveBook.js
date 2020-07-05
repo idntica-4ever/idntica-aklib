3f9c790fd630e7c808449cc62f6e682965d58fee
rjoealexsj <rjoealexander@gmail.com>
2020-07-05 18:20:21 +0530

    rough draft commit

--- src/reductcomponents/pages/RemoveBook.js
+++ src/reductcomponents/pages/RemoveBook.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Bookquery from '../../components/Bookquery';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 //import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {Table} from 'react-bootstrap';
-
 
 import {
     Button,
@@ -21,53 +22,72 @@ import {
     Row,
   } from 'reactstrap';
   const userquery='';
-
-  
+ 
 const config = require('../../config.json');
+
 export default class RemoveBook extends Component {
 
-
-
-     
-  handleremovebook = async(accession_no, event) => {
-
-    event.preventDefault();
-    console.log ("Book Query Received", accession_no);
-     try {
-       const params = {
-         "Accession_No": accession_no,
-         "Book_Status":"Available",
-         "issued_by":"",
-         "issued_on":"",
-         "email_id":"NA"
-       };
-       console.log("Fetching API");
+    state = {
+        newquery: {
+            "email_id": "",
+            "accession_no":""
+        },
+        queries: [],
+        booklst: []
+      }
+    
+    // upercase function
+      toUpperCase = () => {
+        const upperCase = this.state.text.toUpperCase();
+        this.setState({
+            text: upperCase
+        });
+    }
       
-       await axios.patch(`${config.api.invokeUrl}/books/bookings/transaction/${accession_no}`, params);
-       alert ("Book Removed Successfully");
-     } catch (error) {
-       console.log(`An error has occurred: ${error}`);
-     }
+   
+     handlereturnbook = async(accession_no, event) => {
 
-  }
+        event.preventDefault();
+        console.log ("Book Query Received", accession_no);
+         try {
+           const params = {
+             "Accession_No": accession_no,
+             "Book_Status":"Available",
+             "issued_by":"",
+             "issued_on":"",
+             "email_id":"NA"
+           };
+           console.log("Fetching API");
+          
+           await axios.patch(`${config.api.invokeUrl}/books/bookings/transaction/${accession_no}`, params);
+           alert ("Book Returned Successfully");
+         } catch (error) {
+           console.log(`An error has occurred: ${error}`);
+         }
+
+      }
 
 
-    onAddAccessionNoChange = event => this.setState({ newquery: { ...this.state.newquery, 
-      "accession_no": event.target.value } });
-
-
-
+        onAddAccessionNoChange = event => this.setState({ newquery: { ...this.state.newquery, 
+          "accession_no": event.target.value } });
+      
 
     render() {
+          
+        const booklist = this.state.queries && this.state.queries.length > 0
+        ?this.state.queries.map(searchresult => <Bookquery Book_Title= {searchresult.Book_Title} 
+          Book_Author={searchresult.Book_Author} Book_Classification_No={searchresult.Book_Classification_No} Book_Status={searchresult.Book_Status} Book_Scope={searchresult.Book_Scope} key={searchresult.author_title} />)
+        : <div className="tile notification is-warning">NO BOOKS / AUTHOR found.... Try again...</div>   
         return (
-            <div>
 
-<Row>
+            
+            <div>
+                <Row>
         <Col xl={12} lg={12} md={12}>
           <Card>
-            <CardHeader>Remove Book</CardHeader>
+            <CardHeader>User Details</CardHeader>
             <CardBody>
-              <Form onSubmit={event => this.handleremovebook(this.state.newquery.accession_no, event)}>
+              <Form onSubmit={event => this.handlereturnbook(this.state.newquery.accession_no, event)}>
                 
              
 
@@ -100,10 +120,7 @@ export default class RemoveBook extends Component {
 
            
       </Row> 
-
-
-</div>
-           
+            </div>
         )
     }
 }
